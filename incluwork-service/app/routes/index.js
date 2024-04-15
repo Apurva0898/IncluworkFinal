f37fbf170b5ca64fcff7e924c6d7b7402074ac43
Soumya Ganesh <ganesh.so@northeastern.edu>
2024-04-15 15:15:26 -0400

    fixing the initialization of user routes

--- incluwork-service/app/routes/index.js
+++ incluwork-service/app/routes/index.js
@@ -1,7 +1,9 @@
 import authRouter from './authRoutes.js';
+import userRouter from './userRoutes.js';
 
 const initializeRoutes = (app) => {
     app.use('/incluwork', authRouter);
+    app.use('/incluwork', userRouter);
 }
 
 export default initializeRoutes;
--- incluwork-service/app/routes/userRoutes.js
+++ incluwork-service/app/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers } from "../controllers/usercontroller";
+import { getAllUsers } from "../controllers/usercontroller.js";
 import jwtAuth from "../lib/jwtAuth.js";
 
 
