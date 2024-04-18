4a3f9c6b7a52cdc56f5d01f91b212ba09445d4b0
Apurva Raj <raj.ap@northeastern.edu>
2024-04-18 01:49:42 -0400

    getting all jobs as admin

--- incluwork-service/app/services/adminService.js
+++ incluwork-service/app/services/adminService.js
@@ -1,6 +1,8 @@
 import Employer from "../models/Employer.js";
 import User from "../models/User.js";
 import JobSeeker from "../models/JobSeeker.js";
+import Job from "../models/Job.js";
+
 
 // Service functions for Employer model
 
@@ -23,3 +25,29 @@ export const fetchAllUsers = async () => {
         throw err;
     }
 };
+
+export const getAllJobs = async () => {
+    try {
+        // Retrieve all jobs from the database
+        const jobs = await Job.find();
+
+        // Map over the retrieved jobs and transform each job object
+        const transformedJobs = jobs.map(job => {
+            // Destructure the job object to extract relevant properties
+            const { _id, ...rest } = job.toObject();
+
+            // Return a new object with "jobId" instead of "_id"
+            return { jobId: _id, ...rest };
+        });
+        
+        // Return the transformed jobs
+        return transformedJobs;
+    } catch (error) {
+        // If an error occurs, throw an error message
+        throw new Error(`Failed to retrieve jobs: ${error.message}`);
+    }
+};
+
+
+
+
